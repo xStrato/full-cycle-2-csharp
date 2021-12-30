d89a3fb334d92c37bf85d7b012c4b99490d2df3b
xStrato <metalofgear@hotmail.com>
2021-12-30 19:24:28 -0300

    update

--- MicroVideosCatalog/src/MicroVideosCatalog.Domain/Entities/Genre.cs
+++ MicroVideosCatalog/src/MicroVideosCatalog.Domain/Entities/Genre.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using MicroVideosCatalog.Domain.Common;
 
 namespace MicroVideosCatalog.Domain.Entities;
@@ -5,14 +6,41 @@ namespace MicroVideosCatalog.Domain.Entities;
 public record Genre : Entity
 {
     public string Name { get; private set; }
+    private IList<Category> _categories { get; set; } = new List<Category>();
+    public ReadOnlyCollection<Category> Categories => new(_categories);
     protected Genre() { }
     public Genre(string name) : base() => Name = name;
     public Genre(Guid id, string name) : base(id) => Name = name;
+    public Genre(string name, IList<Category> categories) : base()
+        => (Name, _categories) = (name, categories);
+    public Genre(Guid id, string name, IList<Category> categories) : base(id)
+        => (Name, _categories) = (name, categories);
 
     public void SetName(string name)
     {
         if (string.IsNullOrEmpty(name))
             throw new ArgumentException("'name' cannot be empty or null");
         Name = name;
+    }
+
+    public void SetCategories(IList<Category> categories)
+    {
+        if (categories is null or { Count: <= 0 })
+            throw new ArgumentException("'categories' cannot be empty or null");
+        _categories = categories;
+    }
+
+    public void AddCategory(Category category)
+    {
+        if (category is null)
+            throw new ArgumentException("'category' cannot be null");
+        _categories.Add(category);
+    }
+
+    public void RemoveCategory(Category category)
+    {
+        if (category is null)
+            throw new ArgumentException("'category' cannot be null");
+        _categories.Remove(category);
     }
 }
--- MicroVideosCatalog/tests/MicroVideosCatalog.Domain.UnitTests/Entities/GenreTests.cs
+++ MicroVideosCatalog/tests/MicroVideosCatalog.Domain.UnitTests/Entities/GenreTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using MicroVideosCatalog.Domain.Entities;
 using Xunit;
@@ -25,12 +27,13 @@ public class GenreTests
     {
         //Arrange
         var (id, name) = (Guid.NewGuid(), "Horror");
-        Genre c = new(id, name);
+        Genre g = new(id, name);
         var errorMsg = "'name' cannot be empty or null";
         //Act
         //Assert
-        c.Should().Invoking(_ => c.SetName(null)).Should().Throw<ArgumentException>().WithMessage(errorMsg);
-        c.Should().Invoking(_ => c.SetName("")).Should().Throw<ArgumentException>().WithMessage(errorMsg);
+        g.Should().NotBeNull();
+        g.Should().Invoking(_ => g.SetName(null)).Should().Throw<ArgumentException>().WithMessage(errorMsg);
+        g.Should().Invoking(_ => g.SetName("")).Should().Throw<ArgumentException>().WithMessage(errorMsg);
     }
 
     [Fact]
@@ -39,13 +42,57 @@ public class GenreTests
     {
         //Arrange
         var (id, name) = (Guid.NewGuid(), "Horror");
-        Genre c = new(id, name);
+        Genre g = new(id, name);
         var errorMsg1 = "'id' cannot be empty or null";
         var errorMsg2 = "'id' cannot have a default value";
         //Act
         //Assert
-        c.Should().Invoking(_ => c.SetId(null)).Should().Throw<ArgumentException>().WithMessage(errorMsg1);
-        c.Should().Invoking(_ => c.SetId("")).Should().Throw<ArgumentException>().WithMessage(errorMsg1);
-        c.Should().Invoking(_ => c.SetId(Guid.Empty)).Should().Throw<ArgumentException>().WithMessage(errorMsg2);
+        g.Should().NotBeNull();
+        g.Should().Invoking(_ => g.SetId(null)).Should().Throw<ArgumentException>().WithMessage(errorMsg1);
+        g.Should().Invoking(_ => g.SetId("")).Should().Throw<ArgumentException>().WithMessage(errorMsg1);
+        g.Should().Invoking(_ => g.SetId(Guid.Empty)).Should().Throw<ArgumentException>().WithMessage(errorMsg2);
+    }
+
+    [Fact]
+    [Trait("Genre", "Domain/Entities")]
+    public void AddCategory_InvalidCategory_ShouldThrowArgumentException()
+    {
+        //Arrange
+        var (id, name) = (Guid.NewGuid(), "Horror");
+        Genre g = new(id, name);
+        var errorMsg1 = "'category' cannot be null";
+        //Act
+        //Assert
+        g.Should().NotBeNull();
+        g.Should().Invoking(_ => g.AddCategory(null)).Should().Throw<ArgumentException>().WithMessage(errorMsg1);
+    }
+
+    [Fact]
+    [Trait("Genre", "Domain/Entities")]
+    public void RemoveCategory_InvalidCategory_ShouldThrowArgumentException()
+    {
+        //Arrange
+        var (id, name) = (Guid.NewGuid(), "Horror");
+        Genre g = new(id, name);
+        var errorMsg1 = "'category' cannot be null";
+        //Act
+        //Assert
+        g.Should().NotBeNull();
+        g.Should().Invoking(_ => g.RemoveCategory(null)).Should().Throw<ArgumentException>().WithMessage(errorMsg1);
+    }
+
+    [Fact]
+    [Trait("Genre", "Domain/Entities")]
+    public void SetCategories_InvalidCategories_ShouldThrowArgumentException()
+    {
+        //Arrange
+        var (id, name) = (Guid.NewGuid(), "Horror");
+        Genre g = new(id, name);
+        var errorMsg1 = "'categories' cannot be empty or null";
+        //Act
+        //Assert
+        g.Should().NotBeNull();
+        g.Should().Invoking(_ => g.SetCategories(new List<Category>())).Should().Throw<ArgumentException>().WithMessage(errorMsg1);
+        g.Should().Invoking(_ => g.SetCategories(null)).Should().Throw<ArgumentException>().WithMessage(errorMsg1);
     }
 }
