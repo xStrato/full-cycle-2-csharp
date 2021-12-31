f27195e8344fd76f75ee6f5affdfc6da01a8ea36
xStrato <metalofgear@hotmail.com>
2021-12-31 12:56:58 -0300

    update

---
+++ MicroVideosCatalog/src/MicroVideosCatalog.Domain/Entities/VideoFile.cs
@@ -0,0 +1,42 @@
+using MicroVideosCatalog.Domain.Common;
+
+namespace MicroVideosCatalog.Domain.Entities;
+
+public record VideoFile : Entity
+{
+    public string Title { get; private set; }
+    public float Duration { get; private set; }
+    public string Url { get; private set; }
+
+    protected VideoFile() : base() { }
+    public VideoFile(string title, float duration) : base()
+    {
+        Title = title;
+        Duration = duration;
+    }
+    public VideoFile(string title, float duration, string url) : base()
+    {
+        Title = title;
+        Duration = duration;
+        Url = url;
+    }
+    public VideoFile(Guid id, string title, float duration) : base(id)
+    {
+        Title = title;
+        Duration = duration;
+    }
+    public VideoFile(Guid id, string title, float duration, string url) : base(id)
+    {
+        Title = title;
+        Duration = duration;
+        Url = url;
+    }
+    public void SetDuration(float duration) => Duration = duration;
+    public void SetUrl(string url) => Url = url;
+    public void SetTitle(string title)
+    {
+        if (string.IsNullOrEmpty(title))
+            throw new ArgumentException($"'{nameof(title)}' cannot be empty or null");
+        Title = title;
+    }
+}
---
+++ MicroVideosCatalog/tests/MicroVideosCatalog.Domain.UnitTests/Entities/VideoFileTests.cs
@@ -0,0 +1,48 @@
+namespace MicroVideosCatalog.Domain.UnitTests.Entities;
+
+public class VideoFileTests
+{
+    [Fact]
+    [Trait("VideoFile", "Domain/Entities")]
+    public void Constructor_ValidParameters_ShouldCreateVideoFile()
+    {
+        //Arrange
+        var (id, title, duaration) = (Guid.NewGuid(), "123456.mp4", 1.30f);
+        //Act
+        var videoFile = new VideoFile(id, title, duaration);
+        //Assert
+        videoFile.Should().NotBeNull();
+    }
+    [Fact]
+    [Trait("VideoFile", "Domain/Entities")]
+    public void SetId_InvalidParameter_ShouldThrowArgumentException()
+    {
+        //Arrange
+        var (id, title, duaration) = (Guid.NewGuid(), "123456.mp4", 1.30f);
+        //Act
+        var vf = new VideoFile(id, title, duaration);
+        var errorMsg1 = "'id' cannot be empty or null";
+        var errorMsg2 = "'id' cannot have a default value";
+        //Act
+        //Assert
+        vf.Should().NotBeNull();
+        vf.Should().Invoking(_ => vf.SetId(null)).Should().Throw<ArgumentException>().WithMessage(errorMsg1);
+        vf.Should().Invoking(_ => vf.SetId("")).Should().Throw<ArgumentException>().WithMessage(errorMsg1);
+        vf.Should().Invoking(_ => vf.SetId(Guid.Empty)).Should().Throw<ArgumentException>().WithMessage(errorMsg2);
+    }
+    [Fact]
+    [Trait("VideoFile", "Domain/Entities")]
+    public void SetTitle_InvalidNullAndEmptyParameter_ShouldThrowArgumentException()
+    {
+        //Arrange
+        var (id, title, duaration) = (Guid.NewGuid(), "123456.mp4", 1.30f);
+        //Act
+        var vf = new VideoFile(id, title, duaration);
+        var errorMsg = "'title' cannot be empty or null";
+        //Act
+        //Assert
+        vf.Should().NotBeNull();
+        vf.Should().Invoking(_ => vf.SetTitle(null)).Should().Throw<ArgumentException>().WithMessage(errorMsg);
+        vf.Should().Invoking(_ => vf.SetTitle("")).Should().Throw<ArgumentException>().WithMessage(errorMsg);
+    }
+}
